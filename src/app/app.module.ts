5435cdcdcf8ca76c5ae8e8aa5e8191a4b31ab142
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-10-30 12:57:18 +0100

    Step 1
    * Install Angular Material, CDK, Flex-Layout
    * Create `AppMaterialModule` with "planned" modules.
    * Add FlexLayoutModule to main module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,49 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {CommonModule} from '@angular/common';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDatepickerModule,
+  MatExpansionModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatSelectModule,
+  MatTabsModule,
+  MatToolbarModule
+} from '@angular/material';
+
 import {AppComponent} from './app.component';
 import {FlightDataService} from './flights/flight-data.service';
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatNativeDateModule,
+  ]
+})
+export class AppMaterialModule {}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    FlexLayoutModule,
+    AppMaterialModule,
   ],
   providers: [FlightDataService],
   bootstrap: [AppComponent]
