46b3b29a60ecdf8c870a836b8734dbbfb9cc6af9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-11-02 11:56:09 +0100

    Step 6

--- src/app/flights/flights.component.ts
+++ src/app/flights/flights.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FlightDataService, FlightResult} from './flight-data.service';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   templateUrl: './flights.component.html',
@@ -17,12 +19,18 @@ export class FlightsComponent {
 
   todayDate = new Date();
 
-  constructor(formBuilder: FormBuilder) {
+  flights: Observable<FlightResult>;
+
+  constructor(formBuilder: FormBuilder, private flightData: FlightDataService) {
     this.formGroup = formBuilder.group({
       origin: ['', Validators.required],
       destination: ['', Validators.required],
       departure: ['', Validators.required],
       return: ['', Validators.required],
     });
+  }
+
+  loadFlights() {
+    this.flights = this.flightData.getFlights(this.formGroup.value);
   }
 }
