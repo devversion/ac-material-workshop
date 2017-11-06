2ce4ae44a528808651f98fa6f9f4a7bd91c16a66
Elad Bezalel <elad.bezalel@gmail.com>
2017-11-06 22:02:10 +0000

    Step 10

--- src/app/flights/flights.component.ts
+++ src/app/flights/flights.component.ts
@@ -2,17 +2,30 @@ import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {FlightDataService, FlightResult} from './flight-data.service';
 import {Observable} from 'rxjs/Observable';
+import {trigger, transition, query, style, animate, stagger} from '@angular/animations';
 
 @Component({
   templateUrl: './flights.component.html',
-  styleUrls: ['./flights.component.scss']
+  styleUrls: ['./flights.component.scss'],
+  animations: [
+    trigger('results', [
+      transition(':enter', [
+        query('mat-card, mat-expansion-panel', [
+          style({opacity: 0, transform: 'translateX(-100%)'}),
+          stagger(30, [
+            animate('.3s ease', style({opacity: 1, transform: 'translateX(0)'}))
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class FlightsComponent {
 
   airports = [
-    { name: 'London', short: 'LHR' },
-    { name: 'Munich', short: 'MUC' },
-    { name: 'Tel Aviv', short: 'TLV'}
+    {name: 'London', short: 'LHR'},
+    {name: 'Munich', short: 'MUC'},
+    {name: 'Tel Aviv', short: 'TLV'}
   ];
 
   formGroup: FormGroup;
